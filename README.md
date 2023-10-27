a2ac7b4988377bcae068ad4c259def04499b1ed7
quarc0o <emmanuelq150@gmail.com>
2023-10-27 23:39:08 +0200

    setup next js

---
+++ README.md
@@ -0,0 +1,11 @@
+Welcome to development of the new ReStore website!
+
+## Getting Started
+
+To run the project, first install packages using the command
+
+`yarn install`
+
+followed by
+
+`yarn dev`
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-red">New restore website! 🥳</h1>
+    </main>
+  );
+}
