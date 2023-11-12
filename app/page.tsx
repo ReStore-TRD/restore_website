47ca0139094d7b5e86c774f9cd32ca11c349b4ab
quarc0o <emmanuelq150@gmail.com>
2023-11-12 13:20:52 +0100

    add illustration

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
+import test from "./assets/test.svg";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-start justify-between p-24 bg-red-200 text-blue">
+    <main className="flex min-h-screen flex-col items-start justify-between p-24 bg-background">
       <div className="text-5xl flex flex-col gap-2">
         <p>REUSE</p>
         <p>RETHINK</p>
         <p className="text-blue-600">RESTORE</p>
       </div>
+      <Image
+        className="absolute right-0 top-0 w-1/2"
+        src={test}
+        alt={""}
+        width={400}
+      />
     </main>
   );
 }
