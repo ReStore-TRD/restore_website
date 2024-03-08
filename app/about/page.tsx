2637b1cda5259700a024cd7939a5e091d7299af5
quarc0o <emmanuelq150@gmail.com>
2024-03-08 21:50:59 +0100

    change background color globally

---
+++ app/about/page.tsx
@@ -0,0 +1,24 @@
+function About() {
+  return (
+    <div>
+      <div>
+        <p>
+          ReStore is a student organization with roots in Trondheim. At ReStore,
+          we work towards upcycling goods, with the goal of creating a greener
+          and more inclusive society. In May 2019, in a collaboration with SiT
+          Bolig, ReStore was set up in an old kindergarten in Moholt student
+          village and accepted its first donations from outgoing students. At
+          the end of each semester, outgoing students discard tonnes of usable
+          goods when they return home from exchange. Every January and August,
+          new students arrive and need to buy new housewares- the same types
+          that were thrown away only months before. This mismatch in timing
+          between supply and demand perpetuates a cycle of waste. ReStore
+          bridges this gap and facilitates the reuse of goods by storing donated
+          goods for incoming students.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
