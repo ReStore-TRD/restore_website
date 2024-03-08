859d239571c6471d8db7492ca4823c68860a79af
quarc0o <emmanuelq150@gmail.com>
2024-03-08 23:17:03 +0100

    default h2 styl

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,7 @@ function About() {
   return (
     <div>
       <div>
+        <h2>Our story</h2>
         <p>
           ReStore is a student organization with roots in Trondheim. At ReStore,
           we work towards upcycling goods, with the goal of creating a greener
@@ -15,6 +16,18 @@ function About() {
           between supply and demand perpetuates a cycle of waste. ReStore
           bridges this gap and facilitates the reuse of goods by storing donated
           goods for incoming students.
+        </p>
+      </div>
+      <div>
+        <h2>Our goals</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
     </div>
