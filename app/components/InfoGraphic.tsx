ff5de1e047660c43af4d66719b0dfff92c9616f6
Emmanuel Quarcoo <39808085+quarc0o@users.noreply.github.com>
2024-03-08 21:44:29 +0100

    Landing page (#56)
    * temp
    
    * add count animation
    
    * mobile hero done
    
    * desktop layout done

--- app/components/InfoGraphic.tsx
+++ app/components/InfoGraphic.tsx
@@ -1,8 +1,30 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
 interface InfoGraphicProps {
   isLeftLayout: boolean;
 }
 
 function InfoGraphic({ isLeftLayout }: InfoGraphicProps) {
+  const { ref, inView, entry } = useInView({
+    threshold: 0.5,
+  });
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (inView) {
+      const interval = setInterval(() => {
+        if (count < 275) {
+          setCount((prevCount) => prevCount + 1);
+        } else {
+          clearInterval(interval);
+        }
+      }, 7);
+      return () => clearInterval(interval);
+    }
+  }, [inView, count]);
+
   return (
     <div className="w-min lg:w-full justify-center">
       <div className="h-[300px] lg:h-[400px] w-min lg:w-auto flex justify-between rounded-full bg-beige-dark lg:mx-40">
@@ -12,7 +34,9 @@ function InfoGraphic({ isLeftLayout }: InfoGraphicProps) {
           } text-white items-center gap-2 justify-center bg-restore-pink aspect-square rounded-full`}
         >
           <div>ReStore saves a total of</div>
-          <div className="text-[64px] lg:text-[128px] font-bold">275</div>
+          <div className="text-[64px] lg:text-[128px] font-bold" ref={ref}>
+            {count}
+          </div>
           <div>tons of CO2 per year!</div>
         </div>
         <div className=""></div>
