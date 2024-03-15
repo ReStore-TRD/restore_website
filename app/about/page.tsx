b599c5df1f8ca5b07da6bc14c8e64d2da458cbeb
Emmanuel Quarcoo <39808085+quarc0o@users.noreply.github.com>
2024-03-15 17:14:23 +0100

    Feature/vercel deploy (#57)
    * change background color globally
    
    * default h2 styl
    
    * fix build errors
    
    * release
    
    * update vercel.json
    
    * update vercel.json
    
    * update vercel.json

---
+++ app/about/page.tsx
@@ -0,0 +1,37 @@
+function About() {
+  return (
+    <div>
+      <div>
+        <h2>Our story</h2>
+        <p>
+          ReStore is a student organization with roots in Trondheim. At ReStore,
+          we work towards upcycling goods, with the goal of creating a greener
+          and more inclusive society. In May 2019, in a collaboration with SiT
+          Bolig, ReStore was set up in an old kindergarten in Moholt student
+          village and accepted its first donations from outgoing students. At
+          the end of each semester, outgoing students discard tonnes of usable
+          goods when they return home from exchange. Every January and August,
+          new students arrive and need to buy new housewares- the same types
+          that were thrown away only months before. This mismatch in timing
+          between supply and demand perpetuates a cycle of waste. ReStore
+          bridges this gap and facilitates the reuse of goods by storing donated
+          goods for incoming students.
+        </p>
+      </div>
+      <div>
+        <h2>Our goals</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ app/faq/page.tsx
@@ -0,0 +1,5 @@
+function page() {
+  return <div>Faq</div>;
+}
+
+export default page;
