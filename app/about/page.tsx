5d019a837bf70e5ba8c2fc2c534c4314243aa3b9
quarc0o <emmanuelq150@gmail.com>
2024-03-20 13:42:17 +0100

    add first part

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,36 +1,51 @@
+import Image from "next/image";
+import topLeft from "../assets/about_page/top-left.svg";
+import sustainabilityGoals from "../assets/about_page/sustainability_goals.png";
+
 function About() {
   return (
-    <div>
-      <div>
-        <h2>Our story</h2>
-        <p>
-          ReStore is a student organization with roots in Trondheim. At ReStore,
-          we work towards upcycling goods, with the goal of creating a greener
-          and more inclusive society. In May 2019, in a collaboration with SiT
-          Bolig, ReStore was set up in an old kindergarten in Moholt student
-          village and accepted its first donations from outgoing students. At
-          the end of each semester, outgoing students discard tonnes of usable
-          goods when they return home from exchange. Every January and August,
-          new students arrive and need to buy new housewares- the same types
-          that were thrown away only months before. This mismatch in timing
-          between supply and demand perpetuates a cycle of waste. ReStore
-          bridges this gap and facilitates the reuse of goods by storing donated
-          goods for incoming students.
-        </p>
+    <>
+      <Image
+        src={topLeft}
+        alt={""}
+        className="hidden md:block -z-50 absolute top-0 left-0 w-1/6 mix-blend-multiply line-animation"
+      />
+      <div className=" flex flex-col gap-16 px-8">
+        <h1 className="mb-0">About ReStore</h1>
+        <div>
+          <h2 className="mb-0">Our story</h2>
+          <p>
+            ReStore is a student organization with roots in Trondheim. At
+            ReStore, we work towards upcycling goods, with the goal of creating
+            a greener and more inclusive society. In May 2019, in a
+            collaboration with SiT Bolig, ReStore was set up in an old
+            kindergarten in Moholt student village and accepted its first
+            donations from outgoing students. At the end of each semester,
+            outgoing students discard tonnes of usable goods when they return
+            home from exchange. Every January and August, new students arrive
+            and need to buy new housewares- the same types that were thrown away
+            only months before. This mismatch in timing between supply and
+            demand perpetuates a cycle of waste. ReStore bridges this gap and
+            facilitates the reuse of goods by storing donated goods for incoming
+            students.
+          </p>
+        </div>
+
+        <div>
+          <h2 className="mb-0">Our goals</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <Image src={sustainabilityGoals} alt={""} />
       </div>
-      <div>
-        <h2>Our goals</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-    </div>
+    </>
   );
 }
 
