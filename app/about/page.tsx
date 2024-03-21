09b6584e42b57b26b7ce11d076258bc4049e81e6
quarc0o <emmanuelq150@gmail.com>
2024-03-21 18:32:16 +0100

    improve layout on page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -40,47 +40,49 @@ function About() {
       <Image
         src={middle}
         alt={""}
-        className="hidden md:block -z-50 absolute top-6/12 right-0 w-1/6 mix-blend-multiply line-animation"
+        className="hidden md:block -z-50 absolute top-[50vh] right-[5vw] w-1/12 mix-blend-multiply line-animation"
       />
-      <div className=" flex flex-col gap-16 px-8 md:px-48 pb-28">
-        <h1 className="mb-0">About ReStore</h1>
-        <div>
-          <h2 className="mb-6">Our story</h2>
-          <p>
-            ReStore is a student organization with roots in Trondheim. At
-            ReStore, we work towards upcycling goods, with the goal of creating
-            a greener and more inclusive society. In May 2019, in a
-            collaboration with SiT Bolig, ReStore was set up in an old
-            kindergarten in Moholt student village and accepted its first
-            donations from outgoing students. At the end of each semester,
-            outgoing students discard tonnes of usable goods when they return
-            home from exchange. Every January and August, new students arrive
-            and need to buy new housewares- the same types that were thrown away
-            only months before. This mismatch in timing between supply and
-            demand perpetuates a cycle of waste. ReStore bridges this gap and
-            facilitates the reuse of goods by storing donated goods for incoming
-            students.
-          </p>
-        </div>
+      <div className="w-full flex justify-center">
+        <div className=" flex flex-col gap-16 px-8 w-[50vw]  pb-28">
+          <h1 className="mb-0">About ReStore</h1>
+          <div>
+            <h2 className="mb-6">Our story</h2>
+            <p>
+              ReStore is a student organization with roots in Trondheim. At
+              ReStore, we work towards upcycling goods, with the goal of
+              creating a greener and more inclusive society. In May 2019, in a
+              collaboration with SiT Bolig, ReStore was set up in an old
+              kindergarten in Moholt student village and accepted its first
+              donations from outgoing students. At the end of each semester,
+              outgoing students discard tonnes of usable goods when they return
+              home from exchange. Every January and August, new students arrive
+              and need to buy new housewares- the same types that were thrown
+              away only months before. This mismatch in timing between supply
+              and demand perpetuates a cycle of waste. ReStore bridges this gap
+              and facilitates the reuse of goods by storing donated goods for
+              incoming students.
+            </p>
+          </div>
 
-        <div>
-          <h2 className="mb-6">Our goals</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
-        <Image src={sustainabilityGoals} alt={""} className="pb-10" />
+          <div>
+            <h2 className="mb-6">Our goals</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <Image src={sustainabilityGoals} alt={""} className="pb-10" />
 
-        <Quote
-          content="“During my time at Restore I have made friends for life!”"
-          author={"- Volunteer name"}
-        />
+          <Quote
+            content="“During my time at Restore I have made friends for life!”"
+            author={"- Volunteer name"}
+          />
+        </div>
       </div>
       <EmblaCarousel slides={imageArray} options={OPTIONS} />
     </>
