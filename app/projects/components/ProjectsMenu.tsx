29c99603641f163f6e3a66f1ef3f760b69fd510e
quarc0o <emmanuelq150@gmail.com>
2024-03-24 23:36:58 +0100

    desktop done

--- app/projects/components/ProjectsMenu.tsx
+++ app/projects/components/ProjectsMenu.tsx
@@ -31,13 +31,13 @@ function ProjectsMenu({ projectGroups }: ProjectsMenuProps) {
     "hover:bg-no-repeat hover:bg-bottom hover:cursor-pointer hover:bg-projects-underline ";
 
   return (
-    <div>
+    <div className="">
       <div className="grid grid-flow-cols-5 grid-flow-rows-3 justify-center">
-        <h1 className="text-4xl font-medium col-start-2 col-span-2 pb-28">
+        <h1 className="text-4xl font-medium col-start-2 col-span-2 pb-28 ml-16">
           Our Projects
         </h1>
 
-        <div className="row-start-2 flex flex-col gap-4 items-center pr-10">
+        <div className="row-start-2 flex flex-col gap-4 items-center ">
           {projectGroups.map((group, index) => {
             return (
               <p
@@ -54,14 +54,13 @@ function ProjectsMenu({ projectGroups }: ProjectsMenuProps) {
             );
           })}
         </div>
-        <div className="col-span-2 w-[500px] pb-28">
+        <div className="col-span-2 max-w-[500px] pb-28 mx-16">
           {renderDescriptionWithLineBreaks(selectedGroup.projectDescription)}
         </div>
-        <div className="relative -top-28 row-start-2 col-start-4 pl-16">
+        <div className="relative -top-28 row-start-2 col-start-4 pl-16 w-44 h-44">
           <Image
             src={selectedGroup.projectIconUrl?.url ?? ""}
-            width={200}
-            height={200}
+            fill={true}
             alt={""}
           />
         </div>
