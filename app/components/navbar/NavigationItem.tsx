57a91d2f92c2747134b4a364551c3575e76b22e6
Dorsa <kabiridorsa@gmail.com>
2024-03-24 19:38:31 +0100

    updating nav bar

--- app/components/navbar/NavigationItem.tsx
+++ app/components/navbar/NavigationItem.tsx
@@ -11,7 +11,7 @@ interface NavigationItemProps {
 
 const NavigationItem: React.FC<NavigationItemProps> = ({ href, text, imageSrc, altText }) => {
   return (
-    <div className="relative group flex flex-col absolute inset-0 flex items-center justify-center">
+    <div className="group flex-col inset-0 flex items-center justify-center">
       <Link href={href} className="relative border-r pr-4 hover:font-bold">
         {text}
         <div className="absolute right-4 left-0 opacity-0 pointer-events-none transition-opacity duration-300 group-hover:opacity-100">
