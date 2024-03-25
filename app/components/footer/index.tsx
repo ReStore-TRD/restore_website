bbe405a2ee7f85756e032e90b62c025ef8aeef9c
quarc0o <emmanuelq150@gmail.com>
2024-03-25 21:01:56 +0100

    finish footer desktop

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,61 +1,50 @@
-import React from "react";
-import Link from "next/link"
+import { FaFacebook, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import Image from "next/image";
-
-import {
-    FaFacebook,
-    FaInstagram,
-    FaLinkedinIn,
-  } from "react-icons/fa";
-  
+import sitLogo from "../../assets/logos/sit.svg";
+import instagramLogo from "../../assets/icons/instagram.svg";
+import facebookLogo from "../../assets/icons/facebook.svg";
 
 const items = [
-    { name: "Instagram", icon: FaInstagram, link: "https://www.instagram.com/restore_trd"},
-    { name: "Facebook", icon: FaFacebook, link: "https://www.facebook.com/ReStore.TRD"},
-    { name: "LinkedIn", icon: FaLinkedinIn, link: "https://www.linkedin.com/company/restore-trd" },
-  ];
-  
+  {
+    name: "Instagram",
+    icon: FaInstagram,
+    link: "https://www.instagram.com/restore_trd",
+  },
+  {
+    name: "Facebook",
+    icon: FaFacebook,
+    link: "https://www.facebook.com/ReStore.TRD",
+  },
+  {
+    name: "LinkedIn",
+    icon: FaLinkedinIn,
+    link: "https://www.linkedin.com/company/restore-trd",
+  },
+];
 
 const Footer = () => {
   return (
-    <>
-      <div className="w-full pt-10 pb-10 items-center opacity-60 bg-background">
-        <div className="container mx-auto px-4 h-full w-11/12">
-            <div className="flex justify-between">
-                <div>
-                  <a className="text-xs">In partnership with: </a>
-                  <Link href="https://www.sit.no/en/reuse">
-                    <Image
-                      src="/images/logos/sit-logo-standard.svg"
-                      alt="Sit"
-                      width={"136"}
-                      height={"54"}
-                      layout= "responsive"
-                    />
-                  </Link>
-                </div>
-
-                <div className="flex items-center space-x-3 text-4xl">
-                  {items.map((x, index) => (
-                    <Link key={index} href={x.link} className="hover:text-white">
-                        <x.icon />
-                    </Link>
-                  ))}
-
-                </div>
-
-                <div className="text-sm text-center">
-
-                    <p> Prestekragevegen 14 <br></br>
-                        7050, Trondheim </p>
-                        <br></br>
-                    <a href="mailto:info@restore-trd.no">info@restore-trd.no</a>
-                </div>
-
-            </div>
+    <div className=" flex justify-between items-center px-28 h-56 text-sm font-thin">
+      <div className="flex flex-col gap-5">
+        In collaboration with
+        <Image src={sitLogo} alt={""} />
+      </div>
+      <div className="flex gap-6">
+        <div className="flex flex-col items-center gap-4">
+          <Image src={instagramLogo} alt={""} />
+          @restore.trdxals
+        </div>
+        <div className="flex flex-col items-center gap-4">
+          <Image src={facebookLogo} alt={""} />
+          ReStore TRD
         </div>
       </div>
-    </>
+      <div>
+        <p>Prestekrageveien 14</p>
+        <p className="mb-4">1050 Trondheim, Norway</p>
+        <p>info@restore.no</p>
+      </div>
+    </div>
   );
 };
 
