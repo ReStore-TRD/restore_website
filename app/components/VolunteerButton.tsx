1e52417cb510148e8504acc722e3f6a369d1c451
Dorsa <kabiridorsa@gmail.com>
2024-05-22 23:57:53 +0200

    Adding the hover effect for primary button

--- app/components/VolunteerButton.tsx
+++ app/components/VolunteerButton.tsx
@@ -1,15 +1,29 @@
 "use client";
 
 import PrimaryButton from "./Buttons/PrimaryButton";
+import ButtonHover from "../assets/navigation_bar/join_us.svg";
+import Image from "next/image";
 
 function VolunteerButton() {
   return (
-    <div className="flex justify-center">
-      <PrimaryButton
-        title="Volunteer with us!"
-        callBack={() => window.open("https://linktr.ee/restore.trd", "_blank")}
-      />
-    </div>
+      <div className="relative group flex flex-col items-end absolute inset-0 justify-center">
+        <div className="inline-flex">
+          <div className="text-white hover:opacity-80 text-xs">
+            <PrimaryButton
+            title="Volunteer with us!"
+            callBack={() => window.open("https://linktr.ee/restore.trd", "_blank")}
+            />
+          </div>
+        </div>
+        <div className="absolute -top-5 right-1 transform translate-x-full opacity-0 pointer-events-none transition-opacity duration-300 group-hover:opacity-100">
+          <Image
+            src={ButtonHover}
+            alt={""}
+            objectFit="contain"
+            className="mix-blend-multiply w-10 h-10"
+          />
+        </div>
+      </div>
   );
 }
 
