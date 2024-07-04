20a3c1bc17d4d9bc256d809618ece6881111bcab
ReStore-TRD <125396071+restore-trd-dev@users.noreply.github.com>
2024-07-04 21:54:50 +0200

    Update faq.vue

--- pages/faq.vue
+++ pages/faq.vue
@@ -125,8 +125,8 @@
           <div class="accordion-item-body-content">
             <h3>When can I donate items</h3>
             <p>
-              You can leave your donations on the backside portch at our Red
-              building at anytime. But it is highly recommended to donate items during opening hours on Tuesdays and Thursdays from 17:00 to 18:00 so that our volunteers can take in the items and organize
+              You can leave your donations on the backside porch at our Red
+              building at anytime. But it is highly recommended to donate items during donation hours on Saturdays from 10:00 to 11:00 so that our volunteers can take in the items and organize
               them quickly.
             </p>
           </div>
